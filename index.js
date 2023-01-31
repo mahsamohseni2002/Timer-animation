7fbf1a7936a802121c7e46930b1995d46e6b4a18
mahsamohseni2002 <mahsa.mohseni.work@gmail.com>
2023-01-31 11:15:51 +0330

    Worked on animation border timer

--- index.js
+++ index.js
@@ -1,44 +1,21 @@
-class Timer {
-    // parameter here is singnals to our DOM
-    constructor(durationInput, startButton, pauseButton){
-     // assign the arguments to instance variables
-     this.durationInput = durationInput;
-     this.startButton = startButton;
-     this.pauseButton = pauseButton;
-     // whenever in the DOM I clicked on start button play the start method.
-     this.startButton.addEventListener('click', this.start);
-     this.pauseButton.addEventListener('click', this.pause)
-    };
-    start = () => {
-        this.tick();
-        this.interval = setInterval(this.tick, 1000);
-    };
-
-    pause = () => {
-        clearInterval(this.interval);
-    };
-
-    tick = () => { 
-        if (this.timeRemaining <= 0 ){
-            this.pause();
-        } else {
-            this.timeRemaining = this.timeRemaining - 1;
-        }
-        
-    };
-
-    get timeRemaining() {
-        return parseFloat(this.durationInput.value);
-    }
-
-    set timeRemaining(time) {
-        this.durationInput.value = time
-    }
-}
-
  const durationInput = document.querySelector('#duration');
  const startButton = document.querySelector('#start');
  const pauseButton = document.querySelector('#pause');
+ const circle = document.querySelector('circle');
 
-  
-const timer = new Timer (durationInput, startButton, pauseButton)
+const perimeter = circle.getAttribute('r') * 2 * Math.PI; 
+circle.setAttribute('stroke-dasharray', perimeter) 
+
+let currentOffset = 0;
+const timer = new Timer (durationInput, startButton, pauseButton, {
+ onStart() {
+   console.log("Timer just started")
+ },
+ onTick() {
+   circle.setAttribute('stroke-dashoffset',currentOffset )
+   currentOffset = currentOffset - 1;
+ },
+ onComplete() {
+   console.log("Timer is completed")
+ }
+});
